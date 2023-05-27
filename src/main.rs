d117eb17bad59ecd62cb49e09816a519a5b5e811
Sean Wilson <spwilson27@gmail.com>
2023-05-26 23:59:09 -0700

    Almost finish parse impl, missing optional kw parse

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 use std::str::FromStr;
 
+// This one will likely be in a separate file and pub.
+#[derive(Debug, PartialEq)]
+enum FieldType {
+    Int32,
+    Int64,
+    Uint32,
+    Uint64,
+    String,
+}
+
 #[derive(Debug, PartialEq, Default)]
 struct Package {}
 #[derive(Debug, PartialEq, Default)]
-struct Message {}
-#[derive(Debug, PartialEq, Default)]
-struct Field {}
+struct Message {
+    fields: Vec<Field>,
+    messages: Vec<Message>,
+}
+#[derive(Debug, PartialEq)]
+struct Field {
+    ftype: FieldType,
+}
 #[derive(Debug, PartialEq, Default)]
 struct Service {
     rpcs: Vec<Rpc>,
 }
 #[derive(Debug, PartialEq, Default)]
-struct Rpc {}
+struct Rpc {
+    name: String,
+    arg_type: String,
+    ret_type: String,
+}
 
 #[derive(Debug, PartialEq)]
 enum TopLevelParse {
@@ -103,22 +122,6 @@ impl<I: Iterator<Item = char>> Parser<I> {
             iterator: i,
             next_char: None,
         }
-    }
-
-    // TODO: Parser will need to do multiple passes:
-    pub fn next_parse(&mut self) -> Option<TopLevelParse> {
-        while let Some(tok) = self.next_non_ws_token() {
-            return match tok {
-                Token::Ident(ident) => match ident.as_str() {
-                    "syntax" => self.parse_syntax(),
-                    "service" => self.parse_service(),
-                    "message" => self.parse_message(),
-                    _ => panic!("Unexpected token"), // TODO handle unexpected token nicely
-                },
-                _ => panic!("Unexpected token"), // TODO handle unexpected token nicely
-            };
-        }
-        None
     }
 
     pub fn next_non_ws_token(&mut self) -> Option<Token> {
@@ -216,6 +219,22 @@ impl<I: Iterator<Item = char>> Parser<I> {
         None
     }
 
+    // TODO: Parser will need to do multiple passes:
+    pub fn next_parse(&mut self) -> Option<TopLevelParse> {
+        while let Some(tok) = self.next_non_ws_token() {
+            return match tok {
+                Token::Ident(ident) => match ident.as_str() {
+                    "syntax" => self.parse_syntax(),
+                    "service" => self.parse_service(),
+                    "message" => self.parse_message(),
+                    _ => panic!("Unexpected token"), // TODO handle unexpected token nicely
+                },
+                _ => panic!("Unexpected token"), // TODO handle unexpected token nicely
+            };
+        }
+        None
+    }
+
     fn parse_syntax(&mut self) -> Option<TopLevelParse> {
         if self.next_non_ws_token() != Some(Token::Equals) {
             todo!() // Error
@@ -243,13 +262,66 @@ impl<I: Iterator<Item = char>> Parser<I> {
         if self.next_non_ws_token() != Some(Token::BraceOpen) {
             todo!() // Error
         }
-        let service = Service::default();
-        // TODO: Now parse rpcs or braceclose
-        todo!();
-        if self.next_non_ws_token() != Some(Token::BraceClose) {
-            todo!() // Error
+        let mut service = Service::default();
+        loop {
+            // Now parse rpcs or braceclose
+            let tok = self.next_non_ws_token();
+            match tok {
+                Some(Token::Ident(maybe_rpc_ident)) => {
+                    if maybe_rpc_ident.as_str() != "rpc" {
+                        todo!() /*Error*/
+                    }
+                    match self.parse_rpc() {
+                        Some(rpc) => service.rpcs.push(rpc),
+                        None => todo!(), // Error
+                    }
+                }
+                Some(Token::BraceClose) => break, // Done parsing
+                _ => todo!(),                     // Error
+            }
         }
         Some(TopLevelParse::Service(service))
+    }
+
+    fn parse_rpc(&mut self) -> Option<Rpc> {
+        let mut rpc = Rpc::default();
+        // Entered after RPC has been parsed
+        match self.next_non_ws_token() {
+            Some(Token::Ident(rpc_name)) => rpc.name = rpc_name,
+            _ => todo!(), // Error
+        }
+        if self.next_non_ws_token() != Some(Token::ParensOpen) {
+            todo!() // Error
+        }
+        match self.next_non_ws_token() {
+            Some(Token::Ident(arg)) => rpc.arg_type = arg,
+            _ => todo!(), // Error
+        }
+        if self.next_non_ws_token() != Some(Token::ParensClose) {
+            todo!() // Error
+        }
+        match self.next_non_ws_token() {
+            Some(Token::Ident(returns_kw)) => {
+                if returns_kw.as_str() != "returns" {
+                    todo!() // Error
+                }
+            }
+            _ => todo!(), // Error
+        }
+        if self.next_non_ws_token() != Some(Token::ParensOpen) {
+            todo!() // Error
+        }
+        match self.next_non_ws_token() {
+            Some(Token::Ident(ret)) => rpc.ret_type = ret,
+            _ => todo!(), // Error
+        }
+        if self.next_non_ws_token() != Some(Token::ParensClose) {
+            todo!() // Error
+        }
+        if self.next_non_ws_token() != Some(Token::Semicolon) {
+            todo!() // Error
+        }
+        Some(rpc)
     }
 
     fn parse_message(&mut self) -> Option<TopLevelParse> {
@@ -260,13 +332,55 @@ impl<I: Iterator<Item = char>> Parser<I> {
         if self.next_non_ws_token() != Some(Token::BraceOpen) {
             todo!() // Error
         }
-        let message = Message::default();
+
+        let mut message = Message::default();
         // TODO: Now parse fields, messages or braceclose
-        todo!();
-        if self.next_non_ws_token() != Some(Token::BraceClose) {
-            todo!() // Error
+
+        loop {
+            let tok = self.next_non_ws_token();
+            match tok {
+                Some(Token::BraceClose) => break,
+                Some(Token::Ident(ident)) => {
+                    // TODO Support optional keyword
+                    if ident.as_str() == "message" {
+                        self.parse_message();
+                    } else {
+                        match self.parse_field_of_type(ident) {
+                            Some(field) => message.fields.push(field),
+                            _ => todo!(), // Fail, we saw text, so a field needed to come.
+                        }
+                    }
+                }
+                _ => todo!(), // Error
+            }
         }
         Some(TopLevelParse::Message(message))
+    }
+
+    fn parse_field_of_type(&mut self, type_name: String) -> Option<Field> {
+        let ftype = match type_name.as_str() {
+            "int32" => FieldType::Int32,
+            "int64" => FieldType::Int64,
+            "uint32" => FieldType::Uint32,
+            "uint32" => FieldType::Uint64,
+            "string" => FieldType::String,
+            _ => todo!(), //Error
+        };
+        let fname = match self.next_non_ws_token() {
+            Some(Token::Ident(fname)) => fname,
+            _ => todo!(), // Error
+        };
+        if self.next_non_ws_token() != Some(Token::Equals) {
+            todo!() // Error
+        }
+        let number = match self.next_non_ws_token() {
+            Some(Token::Number(n)) => n,
+            _ => todo!(), // Error
+        };
+        if self.next_non_ws_token() != Some(Token::Semicolon) {
+            todo!() // Error
+        }
+        Some(Field { ftype })
     }
 }
 
